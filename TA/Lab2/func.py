638b6646cc75f786fd688e4e6878c4603d14d986
feedblackg44 <pilmyh@gmail.com>
2021-02-28 21:27:23 +0200

    lab2 TA completed

---
+++ TA/Lab2/func.py
@@ -0,0 +1,51 @@
+import random
+
+def generate_data(n, gen_type="random"):
+    if gen_type=="best":
+        a = [i+1 for i in range(n)]
+        return a
+    elif gen_type=="worst":
+        a = [i+1 for i in reversed(range(n))]
+        return a
+    else:
+        a = [i+1 for i in range(n)]
+        random.shuffle(a)
+        return a
+
+def bubble_sort(data):
+    counter = 0
+    for i in range(0, len(data) - 1, 1):                        # Проходим по всем элементам массива в колчиестве раз равным количеству элементов
+        for j in range(0, len(data) - 1, 1):                    # Проходим по всем элементам массива
+            counter += 1
+            if (data[j] > data[j + 1]):                         # Проверка, если текщуий элемент больше следующего
+                data[j + 1], data[j] = data[j], data[j + 1]     # Переставляем элементы в таком случае
+    return counter
+
+def bubble_impr_sort(data):
+    counter = 0
+    for i in range(0, len(data) - 1, 1):                        # Проходим по всем элементам массива в колчиестве раз равным количеству элементов
+        swap = False                                            # Флаг для проверки, не перемещались ли элементы
+        for j in range(0, len(data) - i - 1, 1):                # Проходим по всем элементам массива
+            counter += 1
+            if (data[j] > data[j + 1]):                         # Проверка, если текщуий элемент больше следующего
+                data[j + 1], data[j] = data[j], data[j + 1]     # Переставляем элементы в таком случае
+                swap = True
+        if (swap == False):                                     # Если элементы не перемещались - выходим из цикла, т.к. массив сортирован
+            break
+    return counter
+
+def insertion_sort(data):
+    counter = 0
+    for j in range(1, len(data)):           # Проходим все элементы массива, кроме нулевого
+        done = False                        # Переменная для проверки сравнения
+        key = data[j]                       # Ключ - текущий элемент
+        i = j - 1                           # Индекс предыдущего элемента
+        while i >= 0 and data[i] > key:     # Перебираем элементы от ключа в начало, пока ключ меньше текущего элемента
+            data[i + 1] = data[i]           # Присваиваем следующему элементу значение текущего
+            i -= 1
+            counter += 1
+            done = True
+        if i >= 0 and done == False:        # Если сравнение не произошло увеличиваем каунтер на одно сравнение
+            counter += 1
+        data[i + 1] = key                   # Переставляем ключ на нужную позицию
+    return counter
---
+++ TA/Lab2/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random
+#from plot_data import plot_data
+
+import func
+import plot_func as pf
+
+sizes = [10, 100, 1000, 10000]
+types = ["random", "best", "worst"]
+data_plot = {'random': {'bubble':{}, 'insertion':{}, 'bubble_impr':{}}, 
+             'best':   {'bubble':{}, 'insertion':{}, 'bubble_impr':{}},
+             'worst':  {'bubble':{}, 'insertion':{}, 'bubble_impr':{}}}
+
+for n in sizes:
+    print("\nDATA SIZE: ", n)
+    for gen_type in types:
+        print("\n\tDATA TYPE:", gen_type)
+        data = func.generate_data(n, gen_type)
+
+        data_bubble = np.copy(data)
+        bubble_op_count = func.bubble_sort(data_bubble)
+        print ("\tBubble sort operation count:", int(bubble_op_count))
+        data_plot[gen_type]['bubble'][n] = bubble_op_count
+
+        data_bubble_impr = np.copy(data)
+        bubble_impr_op_count = func.bubble_impr_sort(data_bubble_impr)
+        print("\tImproved bubble sort operation count:", int(bubble_impr_op_count))
+        data_plot[gen_type]['bubble_impr'][n] = bubble_impr_op_count
+
+        data_insertion = np.copy(data)
+        insertion_op_count = func.insertion_sort(data_insertion)
+        print("\tInsertion sort operation count:", int(insertion_op_count))
+        data_plot[gen_type]['insertion'][n] = insertion_op_count
+
+for i in data_plot:
+    pf.plot_data(data_plot, logarithmic=True, oneplot=False)
