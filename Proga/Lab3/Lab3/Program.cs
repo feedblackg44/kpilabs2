026c9e133b4078a9eb53a1a35991fac33f2b8eb5
feedblackg44 <pilmyh@gmail.com>
2021-03-15 11:44:37 +0200

    Lab3 proga completed

---
+++ Proga/Lab3/Lab3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using MyClass;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strToText = "";
+            bool stay = true;
+            Console.WriteLine("Type a text:");
+            while (stay)
+            {
+                string strTemp = Console.ReadLine();
+                strToText += strTemp;
+                if (strTemp == "")
+                    stay = false;
+                else
+                    strToText += '\n';
+            }
+            MyText text = new MyText(strToText);
+
+            Console.Clear();
+            
+            int choice = -1;
+            while (true)
+            {
+                Console.WriteLine("Your text:");
+                text.ConsoleWrite();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case (1):
+                        Console.Write("Type number of line to get (0 or less to go to the main menu): ");
+                        int curLine = GetInt();
+                        if (curLine > 0)
+                        {
+                            Console.Write("Choosed line:\n{0}", text[curLine - 1]);
+                            Console.ReadKey();
+                        }
+                        else
+                            choice = -1;
+                        break;
+                    case (2):
+                        Console.Write("Number of consonant letters in text: {0}", text.NumOfConsLetters);
+                        Console.ReadKey();
+                        choice = -1;
+                        break;
+                    case (0):
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        ShowMenu();
+                        Console.Write("Choose a num from 0 to 2: ");
+                        choice = GetInt();
+                        break;
+                }
+
+                Console.Clear();
+            }
+        }
+        static void ShowMenu()
+        {
+            string menu = "Main Menu\n" +
+                          "1 - Get line of text\n" +
+                          "2 - Get amount of consonant letters\n" +
+                          "0 - Exit";
+            Console.WriteLine(menu);
+        }
+        static int GetInt()
+        {
+            int num;
+
+            bool success = Int32.TryParse(Console.ReadLine(), out num);
+            if (!success)
+            {
+                num = -1;
+            }
+
+            return num;
+        }
+    }
+}
---
+++ Proga/Lab3/MyClass/MyText.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace MyClass
+{
+    public class MyText
+    {
+        public char[][] Array { get; private set; }
+        private int numOfConsLetters;
+        public int NumOfConsLetters
+        {
+            get 
+            {
+                return numOfConsLetters;
+            }
+            private set
+            {
+                numOfConsLetters = value;
+            }
+        }
+
+        // Public methods //
+
+        public MyText()
+        {
+            this.Array = null;
+            NumOfConsLetters = 0;
+        }
+
+        public MyText(string strIn)
+        {
+            int amount = CountEndlines(strIn);
+            this.Array = new char[amount][];
+            this.Array[0] = new char[0];
+            int curLine = 0;
+            int curSymbol = 0;
+            for (int i = 0; i < strIn.Length; i++)
+            {
+                if (strIn[i] == '\n')
+                {
+                    curLine++;
+                    curSymbol = 0;
+                    this.Array[curLine] = new char[0];
+                }
+                else
+                {
+                    this.AddEmptySymbol(curLine);
+                    this.Array[curLine][curSymbol] = strIn[i];
+                    curSymbol++;
+                }
+            }
+
+            this.NumOfConsLetters = CountConsLetters(this.Array);
+        }
+        public void ConsoleWrite()
+        {
+            for (int i = 0; i < this.Array.Length; i++)
+            {
+                for (int j = 0; j < this.Array[i].Length; j++)
+                {
+                    Console.Write("{0}", this.Array[i][j]);
+                }
+                if (i != this.Array.Length - 1)
+                    Console.WriteLine();
+            }
+        }
+        public string this[int key]
+        {
+            get 
+            {
+                string strOut = "";
+                for (int i = 0; i < this.Array[key].Length; i++)
+                {
+                    strOut += this.Array[key][i];
+                }
+                return strOut;
+            }
+        }
+
+        // Private methods //
+
+        private static int CountConsLetters(char[][] array)
+        {
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (MatchPattern(array[i][j]))
+                        counter++;
+                }
+            }
+            return counter;
+        }
+        private static bool MatchPattern(char symbol)
+        {
+            string symbols = "qwrtpsdfghjklzxcvbnm";
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                if (symbol == symbols[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void AddEmptySymbol (int curLine)
+        {
+            int length = this.Array[curLine].Length;
+            char[] temp = new char[length + 1];
+            for (int i = 0; i < length; i++)
+            {
+                temp[i] = this.Array[curLine][i];
+            }
+            this.Array[curLine] = new char[length + 1];
+            for (int i = 0; i < length + 1; i++)
+            {
+                this.Array[curLine][i] = temp[i];
+            }
+        }
+        private static int CountEndlines(string strIn)
+        {
+            int counter = 1;
+            for (int i = 0; i < strIn.Length; i++)
+            {
+                if (strIn[i] == '\n')
+                    counter++;
+            }
+            return counter;
+        }
+
+    }
+}
