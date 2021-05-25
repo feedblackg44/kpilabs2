4ecdce9f4f167907b24c98e1e76ebd37c2e0b6fc
feedblackg44 <pilmyh@gmail.com>
2021-05-25 11:42:05 +0300

    chm lab8 completed

--- CHM/Lab8/functions.py
+++ CHM/Lab8/functions.py
@@ -1,23 +1,79 @@
-def RungeKutta(func, y0, a, b, h):
+from math import ceil
+
+def RungeKutta(func, y0, a, b, h0):
     x0 = a
     x = x0
     y = y0
     arrX = []
     arrY = []
-    arrE = []
+    h = h0
     while x <= b:
         k1 = func(x0, y0)
         k2 = func(x0+h/2, y0+h*k1/2)
         k3 = func(x0+h/2, y0+h*k2/2)
         k4 = func(x0+h, y0+h*k3)
         y = y0 + h/6*(k1+2*k2+2*k3+k4)
-        t = abs((k2-k3)/(k1-k2))
-        print(t)
-        e = 1 / 6 * (k1 * y0 + 2 * k2 * y0 + 2 * k3 * y0 + k4 * y0) - (y - y0) / h
-        arrX.append(x)
+        arrX.append(round(x, 6))
         arrY.append(y)
-        arrE.append(e)
         x += h
         y0 = y
         x0 = x
-    return arrX, arrY, arrE
+        t = abs((k2 - k3) / (k1 - k2))
+        if t > 0.04:
+            h /= 2
+    return arrX, arrY
+
+def Adams(func, y0, a, b, h0):
+    h = h0
+    arrX, arrY = RungeKutta(func, y0, a, a+h*3, h)
+    i = 3
+    while arrX[i] <= b:
+        y_ex = arrY[i] + (h/24)*(55*func(arrX[i], arrY[i]) -
+                                 59*func(arrX[i-1], arrY[i-1]) +
+                                 37*func(arrX[i-2], arrY[i-2]) -
+                                 9*func(arrX[i-3], arrY[i-3]))
+        y_cor = arrY[i] + (h/24)*(9*func(arrX[i]+h, y_ex) +
+                                  19*func(arrX[i], arrY[i]) -
+                                  5*func(arrX[i-1], arrY[i-1]) +
+                                  func(arrX[i-2], arrY[i-2]))
+        arrX.append(round(arrX[i] + h, 6))
+        arrY.append(y_cor)
+        i += 1
+        if (abs(y_cor - y_ex) > 0.04):
+            h /= 2
+    arrX.pop(len(arrX)-1)
+    arrY.pop(len(arrY)-1)
+    return arrX, arrY
+
+def calcualteE(arrX1, arrX2, arrY1, arrY2):
+    arrE = []
+    for i in range(len(arrX1)):
+        tempIndex = arrX2.index(arrX1[i])
+        arrE.append(abs(arrY1[i] - arrY2[tempIndex])/(2**4 - 1))
+    return arrE
+
+def beautifulPrint(arr, num):
+    strOut = ""
+    newArr = [[None for i in range(num)] for i in range (ceil(len(arr) / num))]
+    i = 0
+    j = 0
+    k = 0
+    while i < len(arr):
+        if j < len(newArr):
+            if k < len(newArr[j]):
+                newArr[j][k] = arr[i]
+                i += 1
+                k += 1
+            else:
+                k = 0
+                j += 1
+        else:
+            break
+    for g in range(len(newArr[len(newArr) - 1]) - 1, -1, -1):
+        if newArr[len(newArr) - 1][g] == None:
+            newArr[len(newArr) - 1].pop(g)
+    for g in range(len(newArr)):
+        for u in range(len(newArr[g])):
+            strOut += str(newArr[g][u]) + " "
+        strOut += "\n"
+    return strOut
--- CHM/Lab8/main.py
+++ CHM/Lab8/main.py
@@ -1,11 +1,38 @@
-from functions import RungeKutta
-from matplotlib.pyplot import plot
+from functions import RungeKutta, Adams, beautifulPrint, calcualteE
+from math import sin
+from matplotlib.pyplot import plot, show, xlabel, ylabel
 
-func = lambda y, x: 1+1.4*y*sin(x)-y**2
+func = lambda x, y: 1+1.4*y*sin(x)-y**2
 a = 0
 b = 6
 y0 = 0
-h = 0.01
+h = 0.1
 
-arrX, arrY, arrE = RungeKutta(func, y0, a, b, h)
-plot(arrX, arrY)
+rungeX1, rungeY1 = RungeKutta(func, y0, a, b, h)
+rungeX2, rungeY2 = RungeKutta(func, y0, a, b, h/2)
+rungeE = calcualteE(rungeX1, rungeX2, rungeY1, rungeY2)
+adamsX1, adamsY1 = Adams(func, y0, a, b, h)
+adamsX2, adamsY2 = Adams(func, y0, a, b, h/2)
+adamsE = calcualteE(adamsX1, adamsX2, adamsY1, adamsY2)
+strOut = ""
+strOut += "Перший метод (Рунге-Кутта):\n"
+strOut += beautifulPrint(rungeY1, 5)
+strOut += "Другий метод (Адамса):\n"
+strOut += beautifulPrint(adamsY1, 5)
+strOut += "Похибки першого методу (Рунге-Кутта):\n"
+strOut += beautifulPrint(rungeE, 5)
+strOut += "Похибки другого методу (Адамса):\n"
+strOut += beautifulPrint(adamsE, 5)
+file = open("output.txt", "w")
+file.write(strOut)
+file.close()
+plot(rungeX1, rungeY1)
+plot(adamsX1, adamsY1)
+ylabel("y(x)")
+xlabel("x")
+show()
+plot(rungeX1, rungeE)
+plot(adamsX1, adamsE)
+ylabel("e(x)")
+xlabel("x")
+show()
